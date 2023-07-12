fd40b82eb578a3d2dd2496c9facba43b7d977db9
Luke Hamilton <114652307+lukehamilton2000@users.noreply.github.com>
2023-07-12 12:34:18 +0100

    lol this code doesn't even work

--- retrained MediaPipe model/retrained_first_draft.py
+++ retrained MediaPipe model/retrained_first_draft.py
@@ -1,9 +1,16 @@
-from google.colab import files
+#from google.colab import files
 import os
 import json
-import tensorflow as tf
-assert tf.__version__.startswith('2')
+#import tensorflow as tf
+#assert tf.__version__.startswith('2')
 
 from mediapipe_model_maker import object_detector
 
+train_dataset_path = r"E:\University\Bristol\Dissertation\code\retrained MediaPipe model\tomato_flower_dataset\train"
+validation_dataset_path = r"E:\University\Bristol\Dissertation\code\retrained MediaPipe model\tomato_flower_dataset\validation"
 
+
+train_data = object_detector.Dataset.from_coco_folder(train_dataset_path, cache_dir="/tmp/od_data/train")
+validation_data = object_detector.Dataset.from_coco_folder(validation_dataset_path, cache_dir="/tmp/od_data/validation")
+print("train_data size: ", train_data.size)
+print("validation_data size: ", validation_data.size)
